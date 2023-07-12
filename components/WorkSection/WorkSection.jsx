14bc5e1b17c5e650d094b5bb437e65c84bf462ff
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-12 14:28:06 +0530

    commit 4

--- components/WorkSection/WorkSection.jsx
+++ components/WorkSection/WorkSection.jsx
@@ -1,6 +1,38 @@
-const WorkSection=()=>{
-    return<div className="max-w-6xl mx-auto p-20 w-full">
+import projectsList from "@components/WorkSection/projects";
+import Image from "next/image";
+import Link from "@node_modules/next/dist/client/link";
+import {PiArrowRightThin} from "@node_modules/react-icons/pi";
+
+const WorkSection = () => {
+    return <div className="max-w-6xl mx-auto py-20 px-10 w-full space-y-12">
         <h1 className="text-center text-4xl font-light">Projects</h1>
+        <div className="flex flex-col gap-y-12">
+            {projectsList.map(project => {
+                return <div
+                    className="flex flex-col gap-6 p-4  rounded-2xl lg:flex-row justify-between items-center border border-transparent hover:border-neutral-700 duration-200 transition-all"
+                    key={project.id}>
+                    <div className="h-full space-y-4">
+                        <h1 className="text-4xl">{project.title}</h1>
+                        <p className="text-sm font-light max-w-3xl">{project.description}</p>
+                        <div className="mt-6 gap-2 items-center justify-center inline-flex">
+                            <Link href={project.url} target={"_blank"}
+                                  className=" duration-200 transition-all text-sm underline flex gap-2 items-center">
+                                live site
+                                <PiArrowRightThin size={20}/>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="">
+                        <Link href={project.url} target={"_blank"}>
+                            <Image
+                                className=" duration-200 transition-all min-w-[330px] lg:w-[330px] grayscale hover:grayscale-0 border-none rounded-xl"
+                                src={project.picture} alt={project.title}
+                                width={500} height={200}/>
+                        </Link>
+                    </div>
+                </div>
+            })}
+        </div>
     </div>
 }
 
---
+++ components/WorkSection/projects.js
@@ -0,0 +1,45 @@
+import rudramdevi from "../../public/rudramadevi.png"
+import nikestore from "../../public/nikestore.png"
+import lengthconverter from "../../public/lengthconverter.png"
+import nextjssample from "../../public/nextjssample.png"
+
+
+const projectsList = [
+    {
+        id: 1,
+        title: "Rudramadevi",
+        description: "A comprehensive hostel management app using\n" +
+            "agile methodology to automate room allocation, attendance\n" +
+            "tracking, fee management, and complaint handling, resulting\n" +
+            "in a 50% improvement in operational efficiency.\n",
+        picture: rudramdevi,
+        url: "https://token-server.netlify.app"
+    },
+    {
+        id: 2,
+        title: "Nike Store",
+        description: "A fully functional Nike store ecommerce app with\n" +
+            "    advanced features such as product filtering, sorting, review\n" +
+            "system and an easy-to-use admin dashboard and Stripe\n" +
+            "payment integration",
+        picture: nikestore,
+        url: "https://nikestore-thegdp.netlify.app"
+    },
+    {
+        id: 3,
+        title: "Length Converter",
+        description: "Developed and launched a user-friendly length converter app, allowing users to effortlessly convert length measurements between various metrics such as inches, centimeters, and meters.",
+        picture: lengthconverter,
+        url: "https://length-converter-thegdp.netlify.app"
+    },
+    {
+        id: 4,
+        title: "NextJs Sample",
+        description: "Next js sample website created while learning it",
+        picture: nextjssample,
+        url: "https://nextjs-sample-thegdp.netlify.app"
+    },
+]
+
+export default projectsList
+

f380089740e3cb5e32290931ef1deab0ad85e11b
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-15 18:39:46 +0530

    UI Updates Commit 2

--- components/SkillsSection/SkillsSection.jsx
+++ components/SkillsSection/SkillsSection.jsx
@@ -3,7 +3,7 @@ import {AllIcons} from "@icons/allIcons";
 
 const SkillsSection = () => {
     return <div className="bg-[#1A1A1A] w-full flex justify-center items-center py-10">
-        <Marquee speed={100}>
+        <Marquee speed={100} pauseOnHover={true}>
             {AllIcons.map(icon => {
                 return <div className="mx-6 lg:mx-12 flex justify-center items-center gap-4" key={icon.id}>
                     {icon.icon}
